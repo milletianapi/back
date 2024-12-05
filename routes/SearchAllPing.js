eeecbed56a0dd2fe0edd6a48d3805c9c635d2d38
JoGyoungJun <yoop80075@gmail.com>
2024-12-06 03:00:36 +0900

    다시

--- routes/SearchAllPing.js
+++ routes/SearchAllPing.js
@@ -10,7 +10,7 @@ router.get('/', async function(req, res, next) {
   const cycle = Math.floor((now - startOfDay) / CYCLE_DURATION) + 1;
   const isExist = await groupClient.countDocuments({cycle: cycle})
   if (isExist === 0) {
-    const beforeCount = await groupClient.countDocuments({cycle: cycle === 1 ? 40 : cycle})
+    const beforeCount = await groupClient.countDocuments({cycle: cycle === 1 ? 40 : cycle-1})
     res.send({status:"준비중", count: beforeCount}
     )}
   res.send({status:"갱신됨", count: isExist})
