1b1a61139409f98e1654001259d395230b857051
JoGyoungJun <yoop80075@gmail.com>
2024-12-07 17:44:39 +0900

    ui update

--- dbms/total.js
+++ dbms/total.js
@@ -45,7 +45,7 @@ function delay(ms) {
 const KEY = 'live_338dbbe7d121fa299df2de7c28d5d974ada91ff22d2577b0886df5685d3d7ff7efe8d04e6d233bd35cf2fabdeb93fb0d';
 
 const getall = async (cycle) => {
-    cycle = cycle === 1 ? 40 : cycle;
+    cycle = cycle === 1 ? 40 : cycle-1;
     const list = await totalClient.aggregate([
         {
             $group: {
@@ -81,7 +81,7 @@ const getall = async (cycle) => {
         }
     ]).toArray();
     await groupClient.insertMany(list);
-    await groupClient.deleteMany({cycle: cycle-1});
+    await groupClient.deleteMany({cycle: cycle});
 }
 
 
--- routes/test.js
+++ routes/test.js
@@ -6,7 +6,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/*', async function(req, res, next) {
   visit();
-  res.sendFile(path.resolve(__dirname, '../dist3', 'index3.html'));
+  res.sendFile(path.resolve(__dirname, '../dist', 'index.html'));
 });
 
 module.exports = router;
